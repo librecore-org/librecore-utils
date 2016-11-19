8b5aec9e29a0f0a128bcde856d6174c68c35989f
Damien Zammit <damien@zamaudio.com>
2016-11-19 11:38:37 +1100

    cbfstool/: Replace ifdefs with stubbed null fn library
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

---
+++ cbfstool/vboot_justenough/2sha_stub.c
@@ -0,0 +1,11 @@
+#include "vb2_api.h"
+
+/* Stub function */
+int vb2_digest_buffer(const uint8_t *buf,
+		      uint32_t size,
+		      enum vb2_hash_algorithm hash_alg,
+		      uint8_t *digest,
+		      uint32_t digest_size)
+{
+	return VB2_ERROR_MOCK;
+}
---
+++ cbfstool/vboot_justenough/2sha_stub.h
@@ -0,0 +1,6 @@
+/* Stub function prototype */
+int vb2_digest_buffer(const uint8_t *buf,
+		      uint32_t size,
+		      enum vb2_hash_algorithm hash_alg,
+		      uint8_t *digest,
+		      uint32_t digest_size);
