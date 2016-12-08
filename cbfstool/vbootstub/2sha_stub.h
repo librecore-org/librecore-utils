17954a1c5ecd16389b379a7acf7e63d3f875bce1
Damien Zammit <damien@zamaudio.com>
2016-12-09 01:28:30 +1100

    vbootstub: Break out lib for building coreboot
    With minor tweaks to the coreboot codebase, vboot can be totally
    replaced with this stub library, and cbfstool still builds in utils
    using the same set of stubs.
    
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- cbfstool/vbootstub/2sha_stub.h
+++
@@ -1,6 +0,0 @@
-/* Stub function prototype */
-int vb2_digest_buffer(const uint8_t *buf,
-		      uint32_t size,
-		      enum vb2_hash_algorithm hash_alg,
-		      uint8_t *digest,
-		      uint32_t digest_size);
